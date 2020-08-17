2baa4259d1a37bf4a620de125c3b734332ff8beb
cbpark <cbpark@gmail.com>
2020-08-17 17:13:00 +0900

    Add ModelParams and ModularWeights

--- src/HEP/Data/SUSY/Parameters.hs
+++ src/HEP/Data/SUSY/Parameters.hs
@@ -1,31 +1,39 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module HEP.Data.SUSY.Parameters where
 
 import HEP.Data.Constants
 import HEP.Data.Kinematics (Mass (..))
 
--- data ModelParams = ModelParam { _M0   :: Double
---                               , _cHu  :: Double
---                               , _cHd  :: Double
---                               , _cQ   :: Double
---                               , _ctR  :: Double
---                               , _cbR  :: Double
---                               , _cL   :: Double
---                               , _c
---                               , _tanb :: Double
---                               } deriving Show
+data ModelParams = ModelParams { _M0   :: Double
+                               , _c    :: ModularWeights
+                               , _tanb :: Double
+                               } deriving Show
 
--- data ModularWeight = ModularWeight
+data ModularWeights = ModularWeights { _Hu :: Double
+                                     , _Hd :: Double
+                                     , _Q  :: Double
+                                     , _tR :: Double
+                                     , _bR :: Double
+                                     , _L  :: Double
+                                     , _eR :: Double
+                                     } deriving Show
 
-mHiggs :: Double -> Double -> Double -> Double -> Maybe Mass
-mHiggs as tanb muV mSUSY | mhSq <= 0 = Nothing
-                         | otherwise = Just $ Mass (sqrt mhSq)
+mHiggs :: ModelParams -> Double -> Double -> Maybe Mass
+mHiggs ModelParams {..} mu as | mhSq <= 0 = Nothing
+                              | otherwise = Just $ Mass (sqrt mhSq)
   where
     mhSq = mZ2 * cos2b * cos2b + 3.0 / (4 * pi2) * mt2 * mt2 / vEW2 * termT
 
-    cos2b = cos2Beta tanb
-    loopT = log (mSUSY * mSUSY / mt2)
-    aT2 = 2.0  -- aT = At / MSUSY, At ~ M0, MSUSY ~ M0 / sqrt(2), so aT^2 ~ 2.
-    xT = 2.0 * aT2 * (1.0 - aT2 / 12.0)
+    cos2b = cos2Beta _tanb
+
+    mSUSY2 = _M0 * _M0 / 2.0
+    loopT = log (mSUSY2 / mt2)
+
+    aT = _M0 - mu / _tanb
+    aT2 = aT * aT
+    xT = 2.0 * aT2 / mSUSY2 * (1.0 - aT2 / (12.0 * mSUSY2))
+
     termT = 0.5 * xT + loopT
             + (3.0 / 2 * mt2 / vEW2 - 32 * pi * as)
             * (xT * loopT + loopT * loopT) / (16.0 * pi2)
@@ -34,8 +42,8 @@ cos2Beta :: Double -> Double
 cos2Beta tanb = (1.0 - tanbSq) / (1.0 + tanbSq)
   where tanbSq = tanb * tanb
 
-mu :: Double  -- ^ c_{H_d)
-   -> Double  -- ^ m_*
-   -> Double  -- ^ M_0
-   -> Double
-mu cHd mStar m0 = mStar * (mStar / m0) ** (7.0 / 12) / cHd ** (1.0 / 3)
+getMu :: ModelParams
+      -> Double  -- ^ m_*
+      -> Double
+getMu ModelParams {..} mStar =
+    mStar * (mStar / _M0) ** (7.0 / 12) / _Hd _c ** (1.0 / 3)
--- src/mirage2.hs
+++ src/mirage2.hs
@@ -7,6 +7,22 @@ import HEP.Data.SUSY.Parameters
 main :: IO ()
 main = do
     as <- initAlphaS >>= alphasQ mhSM
+    print as
 
-    let mh = mHiggs as 10 1000.0
+    let mu = getMu point1 1000.0
+    print mu
+
+    let mh = mHiggs point1 mu as
     print mh
+
+point1 :: ModelParams
+point1 = ModelParams { _M0 = 1500.0
+                     , _c  = ModularWeights { _Hu = 0.0
+                                            , _Hd = 1.0
+                                            , _Q  = 0.5
+                                            , _tR = 0.5
+                                            , _bR = 0.5
+                                            , _L  = 0.5
+                                            , _eR = 0.5 }
+                     , _tanb = 10.0
+                     }
