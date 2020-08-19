4e0c7809397bdf38b3febddb5c84bfd02923fb26
cbpark <cbpark@gmail.com>
2020-08-19 21:32:44 +0900

    Update ewsb.hs

--- src/ewsb.hs
+++ src/ewsb.hs
@@ -1,14 +1,30 @@
 module Main where
 
+import HEP.Data.AlphaS    (alphasQ, initAlphaS)
+import HEP.Data.Constants (mhSM, mt)
+import HEP.Data.Quark     (mMSbarHeavy)
 import HEP.Data.SUSY
+
+import Data.Maybe
 
 main :: IO ()
 main = do
-    let mH2@(mHu2, mHd2, mu) = getMHParams point1 0 10 3000.0
-    putStrLn $ "mHu2 = " <> show mHu2 <> ", mHd2 = " <> show mHd2
-        <> ", mu = " <> show mu
-    let bH = getBmu mH2 10
-    putStrLn $ "B = " <> show bH
+    -- let mH2@(mHu2, mHd2, mu) = getMHParams' point1 0 10 3000.0
+    -- putStrLn $ "mHu2 = " <> show mHu2 <> ", mHd2 = " <> show mHd2
+    --     <> ", mu = " <> show mu
+    -- let bH = getBmu mH2 10
+    -- putStrLn $ "B = " <> show bH
+
+    alphaS <- initAlphaS
+    (mtMS,    _, _) <- mMSbarHeavy alphaS mt
+    (   _, mbMS, _) <- mMSbarHeavy alphaS mtMS
+    a3 <- alphasQ mtMS alphaS
+
+    let tanb = 11
+        mHparams = getMHParams point1 0 mhSM (mtMS, mbMS) a3 (1e+3, 1e+4) tanb
+        dB = deltaB (fromJust mHparams)
+    print mHparams
+    putStrLn $ "delta B = " ++ show (1.0 / dB)
 
 point1 :: ModularWeights
 point1 = ModularWeights { _cHu = 0.0
