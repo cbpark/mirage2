8171a00511fc368ee47b30ac03ab9eb812b1d278
cbpark <cbpark@gmail.com>
2020-08-19 18:49:43 +0900

    Fix squark mass

--- lib/HEP/Data/SUSY/Squark.hs
+++ lib/HEP/Data/SUSY/Squark.hs
@@ -6,6 +6,8 @@ import HEP.Data.Constants       (mZ2, mb, mt, sinThetaW2)
 import HEP.Data.Kinematics      (Mass (..), massSq)
 import HEP.Data.SUSY.Parameters
 
+import Numeric.RootFinding
+
 mSquark :: Mass    -- ^ m_{~q_R}
         -> Mass    -- ^ quark mass
         -> Double  -- ^ T3(q)
@@ -19,7 +21,7 @@ mSquark :: Mass    -- ^ m_{~q_R}
 mSquark (Mass mqR) mq t3 rq qq ModularWeights {..} tanb m0 mu =
     (Mass (sqrt m1Sq), Mass (sqrt m2Sq))
   where
-    mQ3 = m0 * _cQ
+    mQ3 = m0 * sqrt _cQ
     mQ3Sq = mQ3 * mQ3
     mqRSq = mqR * mqR
     mqSq  = massSq mq
@@ -36,12 +38,12 @@ mSquark (Mass mqR) mq t3 rq qq ModularWeights {..} tanb m0 mu =
 mStop :: ModularWeights -> Double -> Double -> Double -> (Mass, Mass)
 mStop cs@ModularWeights {..} tanb m0 =
     mSquark mtR mt 0.5 (1.0 / tanb) (2.0 / 3) cs tanb m0
-  where mtR = Mass $ _ctR * m0
+  where mtR = Mass $ sqrt _ctR * m0
 
 mSbottom :: ModularWeights -> Double -> Double -> Double -> (Mass, Mass)
 mSbottom cs@ModularWeights {..} tanb m0 =
     mSquark mbR mb (-0.5) tanb (-1.0 / 3) cs tanb m0
-  where mbR = Mass $ _cbR * m0
+  where mbR = Mass $ sqrt _cbR * m0
 
 getMSUSY :: ModularWeights -> Double -> Double -> Double -> Double
 getMSUSY cs tanb m0 mu = sqrt $ mt1 * mt2
@@ -50,3 +52,14 @@ getMSUSY cs tanb m0 mu = sqrt $ mt1 * mt2
 getMSUSY2 :: ModularWeights -> Double -> Double -> Double -> Double
 getMSUSY2 cs tanb m0 mu = 0.5 * (mt1 * mt1 + mt2 * mt2)
   where (Mass mt1, Mass mt2) = mStop cs tanb m0 mu
+
+getM0FromStop :: ModularWeights -> Double -> Double -> Maybe Double
+getM0FromStop cs mstop tanb = do
+    let muF = getMu cs
+        stopF m0 = let (Mass mt1, _) = mStop cs tanb m0 (muF m0)
+                   in mstop - mt1
+
+    let param = RiddersParam 1000 (AbsTol 1e-3)
+    case ridders param (1e+2, 1e+4) stopF of
+        Root m0 -> return m0
+        _       -> Nothing
