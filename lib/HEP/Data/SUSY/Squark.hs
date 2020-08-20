ee51002f774cbc7ec13c92d50be4eaa2f3aeaf7b
cbpark <cbpark@gmail.com>
2020-08-20 16:02:27 +0900

    Clean up codes

--- lib/HEP/Data/SUSY/Squark.hs
+++ lib/HEP/Data/SUSY/Squark.hs
@@ -1,27 +1,31 @@
-{-# LANGUAGE RecordWildCards #-}
-
-module HEP.Data.SUSY.Squark where
+module HEP.Data.SUSY.Squark
+    (
+      mStop
+    , mSbottom
+    , getMSUSY
+    , getMSUSY2
+    , getM0FromStop
+    ) where
 
 import HEP.Data.Constants       (mZ2, mb, mt, sinThetaW2)
 import HEP.Data.Kinematics      (Mass (..), massSq)
-import HEP.Data.SUSY.Parameters
-import HEP.Util (riddersSolver)
+import HEP.Data.SUSY.Parameters (ModularWeights (..), cos2Beta, getMu)
+import HEP.Util                 (riddersSolver)
 
-mSquark :: Mass    -- ^ m_{~q_R}
-        -> Mass    -- ^ quark mass
-        -> Double  -- ^ T3(q)
-        -> Double  -- ^ R(q)
-        -> Double  -- ^ electric charge
+mSquark :: Mass            -- ^ m_{~q_R}
+        -> Mass            -- ^ quark mass
+        -> Double          -- ^ T3(q) : either 1/2 or -1/2
+        -> Double          -- ^ R(q) : either cot(beta) or tan(beta)
+        -> Double          -- ^ electric charge
         -> ModularWeights
-        -> Double  -- ^ tan(beta)
-        -> Double  -- ^ M_0
-        -> Double  -- ^ mu
+        -> Double          -- ^ tan(beta)
+        -> Double          -- ^ M_0
+        -> Double          -- ^ mu
         -> (Mass, Mass)
-mSquark (Mass mqR) mq t3 rq qq ModularWeights {..} tanb m0 mu =
+mSquark (Mass mqR) mq t3 rq qq ModularWeights { _cQ = cQ } tanb m0 mu =
     (Mass (sqrt m1Sq), Mass (sqrt m2Sq))
   where
-    mQ3 = m0 * sqrt _cQ
-    mQ3Sq = mQ3 * mQ3
+    mQ3Sq = m0 * m0 * cQ
     mqRSq = mqR * mqR
     mqSq  = massSq mq
     cos2b = cos2Beta tanb
@@ -35,29 +39,29 @@ mSquark (Mass mqR) mq t3 rq qq ModularWeights {..} tanb m0 mu =
     m2Sq = abs $ 0.5 * (term1 + term2)
 
 mStop :: ModularWeights -> Double -> Double -> Double -> (Mass, Mass)
-mStop cs@ModularWeights {..} tanb m0 =
+mStop cs@ModularWeights { _ctR = ctR } tanb m0 =
     mSquark mtR mt 0.5 (1.0 / tanb) (2.0 / 3) cs tanb m0
-  where mtR = Mass $ sqrt _ctR * m0
+  where mtR = Mass $ sqrt ctR * m0
 
 mSbottom :: ModularWeights -> Double -> Double -> Double -> (Mass, Mass)
-mSbottom cs@ModularWeights {..} tanb m0 =
+mSbottom cs@ModularWeights { _cbR = cbR } tanb m0 =
     mSquark mbR mb (-0.5) tanb (-1.0 / 3) cs tanb m0
-  where mbR = Mass $ sqrt _cbR * m0
+  where mbR = Mass $ sqrt cbR * m0
 
 getMSUSY :: ModularWeights -> Double -> Double -> Double -> Double
-getMSUSY cs tanb m0 mu = sqrt $ mt1 * mt2
-  where (Mass mt1, Mass mt2) = mStop cs tanb m0 mu
+getMSUSY cs tanb m0 mu = sqrt $ mst1 * mst2
+  where (Mass mst1, Mass mst2) = mStop cs tanb m0 mu
 
 getMSUSY2 :: ModularWeights -> Double -> Double -> Double -> Double
-getMSUSY2 cs tanb m0 mu = 0.5 * (mt1 * mt1 + mt2 * mt2)
-  where (Mass mt1, Mass mt2) = mStop cs tanb m0 mu
+getMSUSY2 cs tanb m0 mu = 0.5 * (mst1 * mst1 + mst2 * mst2)
+  where (Mass mst1, Mass mst2) = mStop cs tanb m0 mu
 
 getM0FromStop :: ModularWeights
-              -> Double
-              -> (Double, Double)
-              -> Double
+              -> Double            -- ^ m_{stop}
+              -> (Double, Double)  -- ^ (xlow, xup)
+              -> Double            -- ^ tan(beta)
               -> Maybe Double
 getM0FromStop cs mstop range tanb = do
-    let stopF m0 = let (Mass mt1, _) = mStop cs tanb m0 (getMu cs m0)
-                   in mstop - mt1
+    let stopF m0 = let Mass mst1 = fst $ mStop cs tanb m0 (getMu cs m0)
+                   in mstop - mst1
     riddersSolver stopF range
--- lib/HEP/Util.hs
+++ lib/HEP/Util.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE Strict #-}
+
 module HEP.Util where
 
 import Numeric.RootFinding
