3d00d48a4a85322cb7affe78088f6d8319dfc90e
cbpark <cbpark@gmail.com>
2020-08-21 15:45:00 +0900

    Minor modification

--- lib/HEP/Util.hs
+++ lib/HEP/Util.hs
@@ -5,11 +5,11 @@ module HEP.Util where
 import Numeric.RootFinding
 
 riddersSolver :: (Double -> Double) -> (Double, Double) -> Maybe Double
-riddersSolver f (xlow, xup) = do
+riddersSolver f (xlow, xup) =
     if xup <= xlow
-        then Nothing
-        else do let param = RiddersParam 1000 (AbsTol 1e-3)
-                case ridders param (xlow, xup) f of
-                    Root x       -> return x
-                    NotBracketed -> riddersSolver f (xlow, xup * 0.95)
-                    _            -> Nothing
+    then Nothing
+    else do let param = RiddersParam 1000 (AbsTol 1e-3)
+            case ridders param (xlow, xup) f of
+                Root x       -> return x
+                NotBracketed -> riddersSolver f (xlow, xup * 0.95)
+                _            -> Nothing
